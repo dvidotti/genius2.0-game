49dd85bbb6268994620da0cb4502823ea421e5cd
dvidotti <danilovidotti@gmail>
2019-04-10 11:10:03 -0300

    random melody working

--- genius.js
+++ genius.js
@@ -7,78 +7,85 @@ let notaLa = new Audio ("la.mp3");
 let notesArray = [notaDo, notaMi, notaSol, notaLa];
 let random4 = () => Math.round(Math.random() * 3);
 let showMelody = document.getElementById('show-melody');
-//let playMelody = document.getElementById('play-melody');
-let melody = picSamplers();   
-function startGame() {
-  game.style.display = 'block';
-  playMelody(notesArray);
-}
-
-
-function playMelody(x) {
-  x[0].play();
-  x[0].addEventListener('ended', (e) => { x[1].play(); });
-  x[1].addEventListener('ended', (e) => { x[2].play(); });
-  x[2].addEventListener('ended', (e) => { x[3].play(); });
-
-  // for (i = 0; i < x.length; i += 1){
-  //   console.log(x[i].played)
-  //   x[i].play()
-  //   // x[i].onended = function(){
-  //   //   console.log(`acabou a musica ${i}`)
-  //   }
-  }
-
-
-// Samplers in numbers & and random 3 & showgame div
+// let melody = picSamplers();   
 let game = document.getElementById('show-game');
 game.style.display ='none';
 
-// Build melody with 3 random elements and show it on the DOM
-function picSamplers() {
-  let newMel = [];
-   if (userPoints < 3){
-     for ( let i = 0; i < 3; i += 1){
-       newMel.push(samplers[random4()]);
-      }
-//       playMelody.innerHTML = `${newMel.join(' ')}`;
-       showMelody.innerHTML = `${newMel.join(' ')}`;
-       return newMel;
-      
-   } else if (userPoints < 5){ 
-     for ( let i = 0; i < 4; i += 1){
-        newMel.push(samplers[random4()]);
-      }
-        showMelody.innerHTML = `${newMel.join(' ')}`;
-        return newMel;
-
-   } else if (userPoints < 7){ 
-    for ( let i = 0; i < 5; i += 1){
-       newMel.push(samplers[random4()]);
-     }
-       showMelody.innerHTML = `${newMel.join(' ')}`;
-       return newMel;
-
-   } else if (userPoints < 8){ 
-    for ( let i = 0; i < 6; i += 1){
-       newMel.push(samplers[random4()]);
-     }
-       showMelody.innerHTML = `${newMel.join(' ')}`;
-       return newMel;
-   } else if (userPoints === 8)
-       return "YOU WIN"; 
+function startGame() {
+  game.style.display = 'block';
+  playMelody(picSamplers());
+  // playMelody([notaDo, notaMi, notaSol]);
 }
 
-
-// Compare melodies created by random function with User guess info
-function compareMelodies() {
-  let userLevel1 = document.getElementById('user-shoot').value;
-  console.log(melody, userLevel1)
-  if (parseInt(melody.join('')) === parseInt(userLevel1)) { 
-     userPoints += 1;
-     console.log('batman',userPoints);
-   melody = picSamplers();  
-  } else { 
-    console.log('Game Over');
+function playMelody(x) {
+  let music = x;
+  if (music.length > 0) {
+    music[0].play();
+    music[0].addEventListener(`ended`, (e) => {
+      music.splice(0, 1);
+      playMelody(music);
+    })
   }
 }
+
+// function playMelody(x) {
+//   x[0].play();
+//   console.log(`x[0]`, x[0])
+//   x[0].addEventListener('ended', (e) => { 
+//     x[1].play();
+//     console.log(`x[1]`, x[1]) });
+//   x[1].addEventListener('ended', (e) => { 
+//     x[2].play();
+//     console.log(`x[2]`, x[2]) });
+//   return false;
+// }
+
+// // Build melody with 3 random elements and show it on the DOM
+function picSamplers() {
+  let newMel = [];
+  //  if (userPoints < 3){
+    for ( let i = 0; i < 3; i += 1) {
+      newMel.push(notesArray[Math.round(Math.random() * 3)]);
+    } 
+    return newMel
+    //showMelody.innerHTML = `${newMel.join(' ')}`;
+  //}
+}
+
+//    } else if (userPoints < 5){ 
+//      for ( let i = 0; i < 4; i += 1){
+//         newMel.push(notesArray[random4()]);
+//       }
+//         showMelody.innerHTML = `${newMel.join(' ')}`;
+//         //return newMel;
+
+//    } else if (userPoints < 7){ 
+//     for ( let i = 0; i < 5; i += 1){
+//        newMel.push(notesArray[random4()]);
+//      }
+//        showMelody.innerHTML = `${newMel.join(' ')}`;
+//      //  return newMel;
+
+//    } else if (userPoints < 8){ 
+//     for ( let i = 0; i < 6; i += 1){
+//        newMel.push(notesArray[random4()]);
+//      }
+//        showMelody.innerHTML = `${newMel.join(' ')}`;
+//       // return newMel;
+//    } else if (userPoints === 8)
+//       // return "YOU WIN"; 
+// }
+
+
+// // Compare melodies created by random function with User guess info
+// function compareMelodies() {
+//   let userLevel1 = document.getElementById('user-shoot').value;
+//   console.log(melody, userLevel1)
+//   if (parseInt(melody.join('')) === parseInt(userLevel1)) { 
+//      userPoints += 1;
+//      console.log('batman',userPoints);
+//    melody = picSamplers();  
+//   } else { 
+//     console.log('Game Over');
+//   }
+// }
