8887c6f3aa39f83feb541854ca673d67cdef720c
dvidotti <danilovidotti@gmail>
2019-04-11 13:39:06 -0300

    Bug nota Repetindo

--- genius.js
+++ genius.js
@@ -5,7 +5,7 @@ let notaSol = new Audio ('sol.mp3');
 let notaLa = new Audio ("la.mp3");
 let notesArray = [notaDo, notaMi, notaSol, notaLa];
 let random4 = () => Math.round(Math.random() * 3);
-let melodyTry =[];
+let melodyTry = [];
 let newMel = [];
 let showMelody = document.getElementById('show-melody');
 let game = document.getElementById('show-game');
@@ -19,38 +19,55 @@ function startGame() {
 // Funções que tocam a nota ativadas no botão
 
 function playNotaDo() {
-  notaDo.play();
   melodyTry.push(notaDo)
-  compareMelodies();
+  notaDo.play();
+  notaDo.addEventListener('ended', () => {
+    compareMelodies();
+  });
+  console.log(counter)
+  // levels();
 }
 
 function playNotaMi() {
-  notaMi.play();
+  console.log('vai tocar a nota mi', melodyTry)
   melodyTry.push(notaMi)
-  compareMelodies();
+  notaMi.play();
+  notaMi.addEventListener('ended', () => {
+    compareMelodies();
+  });
+  console.log(counter)
+  // levels();
 }
 
 function playNotaSol() {
-  notaSol.play();
   melodyTry.push(notaSol)
-  compareMelodies();
+  notaSol.play();
+  notaSol.addEventListener('ended', () => {
+    compareMelodies();
+  });
+  console.log(counter)
+  // levels();
 }
 
 function playNotaLa() {
-  notaLa.play();
   melodyTry.push(notaLa)
-  compareMelodies();
+  notaLa.play();
+  notaLa.addEventListener('ended', () => {
+    compareMelodies();
+  });
+  console.log(counter)
+  // levels();
 }
 
 
 function playMelody(x) {
   let music = x.slice();
   if (music.length > 0) {
-    music[0].play();
     music[0].addEventListener(`ended`, (e) => {
       music.splice(0, 1);
       playMelody(music);
     })
+    music[0].play();
   }
 }
 
@@ -58,18 +75,12 @@ function playMelody(x) {
 // // Build melody with 3 random elements and show it on the DOM
 function picSamplers() {
   //  if (userPoints < 3){
-  for ( let i = 0; i < 3; i += 1) {
-    newMel.push(notesArray[Math.round(Math.random() * 3)]);
-  } 
-  playMelody(newMel);
-}
-
-function compareArrays (a,b)
-{ 
-  if (a === b) {
-    return true;
+    for ( let i = 0; i < 3  ; i += 1) {
+      newMel.push(notesArray[Math.round(Math.random() * 3)]);
+    } 
+    playMelody(newMel);
+    //return newMel; 
   }
-}
   
   //    } else if (userPoints < 5){ 
     //      for ( let i = 0; i < 4; i += 1){
@@ -96,33 +107,56 @@ function compareArrays (a,b)
               // }
               
               
- //Compare melodies created by random function with User guess info
+              //Compare melodies created by random function with User guess info
+              
+              let counter = 0;
+              function compareMelodies() {
+                console.log('melodyTry', melodyTry);
+                for (let i = 0; i < melodyTry.length; i += 1) {
+                  if (melodyTry[i].src.toString() === newMel[i].src.toString()) {
+                    counter += 1;
+                  } else {
+                    console.log(`GAME OVER`)
+                  }
+                }
+              }
 
-function compareMelodies() {
-  let z = (...melodyTry);
-  let y = (...newMel);
-  if (melodyTry.length === newMel.length){
-    if(compareArrays(z,y)=== true) {
-   
-      // console.log(...melodyTry, ...newMel);
-    userPoints += 1;
-    melodyTry =[];
-    nelMel = [];
-   //picSamplers();
-  //   } else { 
-  //    console.log('Game Over');
-  //   }
-    }
-  } 
-}
-          
-          
-          
-          
-          // // Compare melodies created by random function with User guess info
-          // function compareMelodies() {
-            //   let userLevel1 = document.getElementById('user-shoot').value;
-            //   console.log(melody, userLevel1)
+              function levels() {
+                if (counter === 3) {
+                  userPoints += 1;
+                  //melodyTry = [];
+                  //newMel = [];
+                  console.log(`levels`)
+                  // return picSamplers();
+                }
+              }
+                // if (counter < newMel.length) {
+                //   for (let i = 0; i < melodyTry.length; i += 1) {
+                //     if (melodyTry[i].src !== newMel[i].src) {
+                //       return console.log('GAME OVER')
+                //     }
+                //   }
+                // }
+                // if (counter === newMel.length) {
+                //   userPoints += 1;
+                //   melodyTry = [];
+                //   newMel = [];
+                //   counter = 0;
+                //   picSamplers();
+                //   return console.log('YOU WIN')
+                //   }
+                //   return counter += 1
+                  
+                
+                
+                
+                
+                
+                
+                // // Compare melodies created by random function with User guess info
+                // function compareMelodies() {
+                  //   let userLevel1 = document.getElementById('user-shoot').value;
+                  //   console.log(melody, userLevel1)
             //   if (parseInt(melody.join('')) === parseInt(userLevel1)) { 
               //      userPoints += 1;
               //      console.log('batman',userPoints);
