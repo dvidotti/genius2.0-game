eda8cebf9d9135a3b8987e4971659739c237ef30
dvidotti <danilovidotti@gmail>
2019-04-11 23:02:52 -0300

    game over ok, falta parar o play melody

--- genius.js
+++ genius.js
@@ -6,6 +6,8 @@ let melodyTry = [];
 let newMel = [];
 let playButton = document.querySelectorAll('.btn');
 let game = document.getElementById('show-game');
+let startButton = document.getElementById('start-game');
+let gameOver = document.getElementById('game-over');
 game.style.display ='none';
 
 function startGame() {
@@ -15,9 +17,10 @@ function startGame() {
     e.classList.remove('blue');
     e.classList.remove('green');
    })
-  game.style.display = 'block'; 
+  game.style.display = 'flex'; 
+  startButton.style.display = 'none';
   picSamplers();
-  playMelody();
+  playMelody(); 
 }
 
 function lightButton(z) {
@@ -58,26 +61,30 @@ function playNote(note) { console.log(note);
 
 
 function picSamplers(){
-  if (counter <= 3){
+  if (userPoints <= 2){
    for (let i = 0; i < 3; i += 1) {
      newMel.push(notes[random4()]);
     } 
-} if (counter > 3 &&  counter <= 5){
+} if (userPoints > 2 &&  userPoints <= 4){
     for (let i = 0; i < 5; i += 1) {
      newMel.push(notes[random4()]);
     }
- } if (counter > 5){
-    for (let i = 0; i < 7; i += 1) {
-     newMel.push(notes[random4()]);
-    }
- }
-     
+  } if (userPoints >= 5 && userPoints <= 6){
+     for (let i = 0; i < 7; i += 1) {
+      newMel.push(notes[random4()]);
+     }
+  } if (userPoints >= 7 && userPoints < 9){
+     for (let i = 0; i < 3; i += 1) {
+      newMel.push(notes[random4()]);
+     } 
+  } if (userPoints === 9) {
+    return console.log('YOU WIN');
+  }
 }
-
 
 // cria as notas da melodia, usando a string (c, e, g, a), a cada loop pega a string da melodia newMel e push para audioSeq... dúvidas no segund 'for' Acho que o audioSeq[0] dispara a melodia, que por sua vez dispara as outras com o addEventeListener
 
-function playMelody() {
+function playMelody() { 
   const audioSeq = [];
   for (let i = 0; i < newMel.length; i += 1) {
     const note = new Audio(`${newMel[i]}.mp3`);
@@ -88,7 +95,7 @@ function playMelody() {
       audioSeq[i].addEventListener('ended', () => {
 
         audioSeq[i + 1].play();
-        if (userPoints < 2){
+        if (userPoints <=7){
         if (audioSeq[i + 1].src === 'file:///Users/danilovidotti/Documents/CODE%20PROJECTS/ironhack/Projetc1/c.mp3'){ 
           playButton[0].classList.add('red')
           setTimeout(() => {  
@@ -117,7 +124,7 @@ function playMelody() {
 
 
   audioSeq[0].play();
-   if (userPoints < 2){
+   if (userPoints <= 7){
     if (audioSeq[0].src === 'file:///Users/danilovidotti/Documents/CODE%20PROJECTS/ironhack/Projetc1/c.mp3'){ 
       playButton[0].classList.add('red')
       setTimeout(() => {  
@@ -143,15 +150,20 @@ function playMelody() {
 }
 
 
-
-
+let gameFinish = false;
 
 function compareMelodies(){
   console.log(newMel, melodyTry);
   if (newMel.length === melodyTry.length){
    for (let i = 0; i < newMel.length; i += 1)
       if (newMel[i] !== melodyTry[i]) {
-       console.log(`GAME OVER`)
+        newMel = [];
+        melodyTry = [];
+        userPoints = 0;
+        counter = 0;
+        gameOver.style.display = 'flex';
+        game.style.display ='none';
+        
     } if (newMel.length === melodyTry.length){ 
       counter += 1;
       userPoints += 1;
@@ -159,8 +171,7 @@ function compareMelodies(){
       melodyTry = []; 
       picSamplers();
       setTimeout(playMelody,1000);  
-     // playMelody();
-    } //return console.log(counter) ;
+    } 
  }
 }
 
